3860c2723468a4421fff97e7eaffcd9784945c60
JordanS505 <166937878+JordanS505@users.noreply.github.com>
2025-07-03 10:29:29 +0200

    aggiunta tipologia

--- src/main/java/org/elis/model/Tipologia.java
+++ src/main/java/org/elis/model/Tipologia.java
@@ -2,4 +2,18 @@ package org.elis.model;
 
 public class Tipologia {
 
+	private String nome;
+
+	public Tipologia(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 }
