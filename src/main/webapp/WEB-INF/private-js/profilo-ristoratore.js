08429250eb5566135726828b8b66cafb7e33be3d
anthosempli <anthosempli@gmail.com>
2025-07-09 12:42:05 +0200

    aggiunta pf ristoratore

---
+++ src/main/webapp/WEB-INF/private-js/profilo-ristoratore.js
@@ -0,0 +1,39 @@
+// Hamburger menu toggle
+const hamburger = document.getElementById('hamburger');
+const navmenu = document.getElementById('navmenu');
+
+hamburger.addEventListener('click', () => {
+  navmenu.classList.toggle('active');
+});
+
+// Chiudi menu al click su link (opzionale)
+document.querySelectorAll('.navmenu a').forEach(link => {
+  link.addEventListener('click', () => {
+    navmenu.classList.remove('active');
+  });
+});
+
+
+// SLIDER CAROSELLO ORDINI
+const carousel = document.getElementById('carouselOrders');
+    const scrollAmount = 520;
+
+    document.getElementById('scrollLeft').addEventListener('click', () => {
+        carousel.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    });
+
+    document.getElementById('scrollRight').addEventListener('click', () => {
+        carousel.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    });
+
+// SLIDER CAROSELLO ORDINI IN PREPARAZIONE
+const carouselPreparing = document.getElementById('carouselPreparing');
+const scrollAmount2 = 520;
+
+document.getElementById('scrollLeft2').addEventListener('click', () => {
+    carouselPreparing.scrollBy({ left: -scrollAmount2, behavior: 'smooth' });
+});
+
+document.getElementById('scrollRight2').addEventListener('click', () => {
+    carouselPreparing.scrollBy({ left: scrollAmount2, behavior: 'smooth' });
+});
